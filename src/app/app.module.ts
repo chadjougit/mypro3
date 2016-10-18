ff1d480d7577cea447f04517a2accf899d0a3f62
chadjougit <deftones@protonmail.com>
2016-10-18 15:47:55 +0300

    two

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MycompComponent } from './mycomp/mycomp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MycompComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mycomp/mycomp.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { MycompComponent } from './mycomp.component';
+
+describe('Component: Mycomp', () => {
+  it('should create an instance', () => {
+    let component = new MycompComponent();
+    expect(component).toBeTruthy();
+  });
+});
